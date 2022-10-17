2b7bb24733fb377870872b65ed4c3f68aaa5c268
System Administrator <root@macbooks-MacBook-Pro.local>
2022-10-17 12:30:15 +0700

    fixing response create product

--- product/src/api/controllers/auth.js
+++ product/src/api/controllers/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken')
-
+const dotenv = require('dotenv')
+dotenv.config()
 exports.login = (request,h) => {
     const username = "testing"
     const password = "password"
@@ -7,7 +8,7 @@ exports.login = (request,h) => {
         access_token:""
     }
    
-    return jwt.sign({ username: username }, "jwt_secrete", { algorithm: "HS256",keyid: "sim2" })
+    return jwt.sign({ username: username }, process.env.JWT_SECRETE || "jwt_secrete", { algorithm: "HS256",keyid: "sim2" })
 
     
 }
